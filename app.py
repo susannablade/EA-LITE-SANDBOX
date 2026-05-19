7a28f51b65eef7ad775186c7be5892d4534c4200
Susanna Dancy <sbdancy@gmail.com>
2026-05-19 09:55:10 -0400

    updating genai model

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def ask_gemini(prompt):
     try:
 
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-3.1-flash-lite"",
             contents=prompt
         )
 
